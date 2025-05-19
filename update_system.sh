c0488a567407ad36d6ec044a0aedb4b3dcb4af50
Jorge Garrido Zarzoso <84621249+JorgeGarrido98@users.noreply.github.com>
2025-05-19 12:34:33 +0200

    Update update_system.sh

--- update_system.sh
+++ update_system.sh
@@ -9,16 +9,17 @@ echo
 # Código del script
 echo "IMPORTANTE EJECUTAR CON PERMISOS DE ROOT"
 sleep 2
-read -p "Si quieres actualizar tu sistema escribe 'Actualizar' y si quieres salir escribe 'Salir': " respuesta
+echo -e "1) Actualizar \n2) Salir"
+read -p "Respuesta: " respuesta
 
 if [ "$respuesta" = "Actualizar" ]; then
-	apt update
-	apt upgrade -y #El parámetro -y sirve para automatizar la respuesta de Si o No para actualizar el sistema
-	apt autoremove -y
-	apt autoclean -y
-	echo
-	echo "================================================================"
-	echo "El sistema entero ha sido actualizado"
+        apt update
+        apt upgrade -y #El parámetro -y sirve para automatizar la respuesta de Si o No para actualizar el sistema
+        apt autoremove -y
+        apt autoclean -y
+        echo
+        echo "================================================================"
+        echo "El sistema entero ha sido actualizado"
 elif [ "$respuesta" = "Salir" ]; then
-	exit
+        exit
 fi
